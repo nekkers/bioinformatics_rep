23466c4fa644d18ac15a367066589ce1150866f3
nekkers <nikita.al.pogodin@gmail.com>
2025-05-22 18:48:06 +0300

    New functions

---
+++ main.py
@@ -0,0 +1,34 @@
+from json import *
+from Bio import Align
+def initailize(path1, path2):
+    with open(path1, 'r') as js:
+        gd = load(js)
+    with open(path2, 'r') as js:
+        md = load(js)
+    return gd, md
+def convert(gd, md):
+    gl, ml = [], []
+    for k1, v1 in gd.values():
+        gl.append(v1)
+    for k2, v2 in md.values():
+        ml.append(v2)
+    return gl, ml
+def align_two(s1, s2):
+    aligner = Align.PairwiseAligner()
+    aligner.mode = "local"
+    alignments = aligner.align(s1, s2)
+    return alignments
+def align_full(gl, ml):
+    fl = []
+    for g in gl:
+        for m in ml:
+            fl.append('Aligned: ' + g + ' and '+ m)
+            als = align_two(g, m)
+            for a in als:
+                fl.append(a)
+    return fl
+def writing(expath, fl):
+    f = open(expath, 'w')
+    for el in fl:
+        f.write(el)
+    f.close()
